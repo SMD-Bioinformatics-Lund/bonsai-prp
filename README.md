b686ac395dd0a6a44c00a03abb1c1520babccf7c
mhkc <markus.johansson@me.com>
2023-11-28 12:17:07 +0100

    Added prp from JASEN repo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# prp
-Pipeline output Processing Program for Bonsai
+# Pipeline result processor
+
+The Pipeline Result Processor, PRP, is a collection of parsers and data models for creation and validation of a standardized output for the JASEN pipeline.
+
+## Installation
+
+The script requires python v3.10 or later.
---
+++ prp/models/base.py
@@ -0,0 +1,14 @@
+"""Generic database objects of which several other models are based on."""
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseConfig, BaseModel
+
+
+class RWModel(BaseModel):
+    """Base model for read/ write operations"""
+
+    class Config(BaseConfig):
+        allow_population_by_alias = True
+        populate_by_name = True
+        use_enum_values = True
