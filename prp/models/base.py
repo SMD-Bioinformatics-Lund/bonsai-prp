fd3f518d74f0c387cab8fe60d44db3cde4529510
mhkc <markus.johansson@me.com>
2023-12-04 13:31:50 +0100

    replaced deprecated pydantic Config with ConfigDict

--- prp/models/base.py
+++ prp/models/base.py
@@ -1,13 +1,12 @@
 """Generic database objects of which several other models are based on."""
-from pydantic import BaseConfig, BaseModel
+from pydantic import ConfigDict, BaseModel
 
 
 class RWModel(BaseModel):  # pylint: disable=too-few-public-methods
     """Base model for read/ write operations"""
 
-    class Config(BaseConfig):  # pylint: disable=too-few-public-methods
-        """Configure base model behaviour."""
-
-        allow_population_by_alias = True
-        populate_by_name = True
-        use_enum_values = True
+    model_config = ConfigDict(
+        allow_population_by_alias = True,
+        populate_by_name = True,
+        use_enum_values = True,
+    )
