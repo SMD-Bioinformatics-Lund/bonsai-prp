75a65e7346adf9d5114b3f0b93262d61a8d1d2f0
mhkc <markus.johansson@me.com>
2023-12-07 10:52:30 +0100

    removed debug statement

--- prp/parse/phenotype/resfinder.py
+++ prp/parse/phenotype/resfinder.py
@@ -93,25 +93,22 @@ def _parse_resfinder_amr_genes(
         category = ElementType(res_category)
 
         # store results
-        try:
-            gene = ResistanceGene(
-                gene_symbol=info["name"],
-                accession=info["ref_acc"],
-                depth=info["depth"],
-                identity=info["identity"],
-                coverage=info["coverage"],
-                ref_start_pos=info["ref_start_pos"],
-                ref_end_pos=info["ref_end_pos"],
-                ref_gene_length=info["ref_seq_length"],
-                alignment_length=info["alignment_length"],
-                phenotypes=info["phenotypes"],
-                ref_database=info["ref_database"][0],
-                ref_id=info["ref_id"],
-                element_type=category,
-                element_subtype=_assign_res_subtype(info, category),
-            )
-        except:
-            import pdb; pdb.set_trace()
+        gene = ResistanceGene(
+            gene_symbol=info["name"],
+            accession=info["ref_acc"],
+            depth=info["depth"],
+            identity=info["identity"],
+            coverage=info["coverage"],
+            ref_start_pos=info["ref_start_pos"],
+            ref_end_pos=info["ref_end_pos"],
+            ref_gene_length=info["ref_seq_length"],
+            alignment_length=info["alignment_length"],
+            phenotypes=info["phenotypes"],
+            ref_database=info["ref_database"][0],
+            ref_id=info["ref_id"],
+            element_type=category,
+            element_subtype=_assign_res_subtype(info, category),
+        )
         results.append(gene)
     return results
 
