0b37a9563d91766f1d96b3f0fd6d8176570b1327
ryanjameskennedy <ryanjameskennedy@icloud.com>
2023-12-29 15:55:24 +0100

    Add Mtuberculosis test files

---
+++ tests/fixtures/mtuberculosis/__init__.py
@@ -0,0 +1,35 @@
+"""mtuberculosis input data fixutres."""
+
+import pytest
+
+from ..fixtures import data_path
+
+
+@pytest.fixture()
+def mtuberculosis_analysis_meta_path(data_path):
+    return str(data_path.joinpath("mtuberculosis", "analysis_meta.json"))
+
+
+@pytest.fixture()
+def mtuberculosis_bracken_path(data_path):
+    return str(data_path.joinpath("mtuberculosis", "bracken.out"))
+
+
+@pytest.fixture()
+def mtuberculosis_bwa_path(data_path):
+    return str(data_path.joinpath("mtuberculosis", "bwa.qc"))
+
+
+@pytest.fixture()
+def mtuberculosis_mykrobe_path(data_path):
+    return str(data_path.joinpath("mtuberculosis", "mykrobe.csv"))
+
+
+@pytest.fixture()
+def mtuberculosis_quast_path(data_path):
+    return str(data_path.joinpath("mtuberculosis", "quast.tsv"))
+
+
+@pytest.fixture()
+def mtuberculosis_tbprofiler_path(data_path):
+    return str(data_path.joinpath("mtuberculosis", "tbprofiler.json"))
