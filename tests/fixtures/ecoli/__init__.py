4b77020ef5809c15db59afad0b70140c89e40c66
ryanjameskennedy <ryanjameskennedy@icloud.com>
2024-01-02 23:00:43 +0100

    Fix data_path error

--- tests/fixtures/ecoli/__init__.py
+++ tests/fixtures/ecoli/__init__.py
@@ -4,87 +4,90 @@ import json
 
 import pytest
 
+from ..fixtures import data_path
+
+
 @pytest.fixture()
-def ecoli_analysis_meta_path(data_path):
+def ecoli_analysis_meta_path(data_fpath):
     """Get path for ecoli meta file"""
-    return str(data_path.joinpath("ecoli", "analysis_meta.json"))
+    return str(data_fpath.joinpath("ecoli", "analysis_meta.json"))
 
 
 @pytest.fixture()
-def ecoli_bwa_path(data_path):
+def ecoli_bwa_path(data_fpath):
     """Get path for ecoli bwa qc file"""
-    return str(data_path.joinpath("ecoli", "bwa.qc"))
+    return str(data_fpath.joinpath("ecoli", "bwa.qc"))
 
 
 @pytest.fixture()
-def ecoli_quast_path(data_path):
+def ecoli_quast_path(data_fpath):
     """Get path for ecoli quast file"""
-    return str(data_path.joinpath("ecoli", "quast.tsv"))
+    return str(data_fpath.joinpath("ecoli", "quast.tsv"))
 
 
 @pytest.fixture()
-def ecoli_amrfinder_path(data_path):
+def ecoli_amrfinder_path(data_fpath):
     """Get path for ecoli amrfinder file"""
-    return str(data_path.joinpath("ecoli", "amrfinder.out"))
+    return str(data_fpath.joinpath("ecoli", "amrfinder.out"))
 
 
 @pytest.fixture()
-def ecoli_resfinder_path(data_path):
+def ecoli_resfinder_path(data_fpath):
     """Get path for ecoli resfinder file"""
-    return str(data_path.joinpath("ecoli", "resfinder.json"))
+    return str(data_fpath.joinpath("ecoli", "resfinder.json"))
 
 
 @pytest.fixture()
-def ecoli_resfinder_meta_path(data_path):
+def ecoli_resfinder_meta_path(data_fpath):
     """Get path for ecoli resfinder meta file"""
-    return str(data_path.joinpath("ecoli", "resfinder_meta.json"))
+    return str(data_fpath.joinpath("ecoli", "resfinder_meta.json"))
 
 
 @pytest.fixture()
-def ecoli_virulencefinder_wo_stx_path(data_path):
+def ecoli_virulencefinder_wo_stx_path(data_fpath):
     """Get path for ecoli virulencefinder without stx file"""
-    return str(data_path.joinpath("ecoli", "virulencefinder.json"))
+    return str(data_fpath.joinpath("ecoli", "virulencefinder.json"))
 
 
 @pytest.fixture()
-def ecoli_virulencefinder_stx_pred_stx_path(data_path):
+def ecoli_virulencefinder_stx_pred_stx_path(data_fpath):
     """Get path for ecoli stx prediction file"""
-    return str(data_path.joinpath("ecoli", "virulencefinder.stx_pred.stx.json"))
+    return str(data_fpath.joinpath("ecoli", "virulencefinder.stx_pred.stx.json"))
 
 
 @pytest.fixture()
-def ecoli_virulencefinder_stx_pred_no_stx_path(data_path):
+def ecoli_virulencefinder_stx_pred_no_stx_path(data_fpath):
     """Get path for ecoli stx prediction no stx file"""
-    return str(data_path.joinpath("ecoli", "virulencefinder.stx_pred.no_stx.json"))
+    return str(data_fpath.joinpath("ecoli", "virulencefinder.stx_pred.no_stx.json"))
 
 
 @pytest.fixture()
-def ecoli_virulencefinder_meta_path(data_path):
+def ecoli_virulencefinder_meta_path(data_fpath):
     """Get path for ecoli virulencefinder meta file"""
-    return str(data_path.joinpath("ecoli", "virulencefinder_meta.json"))
+    return str(data_fpath.joinpath("ecoli", "virulencefinder_meta.json"))
 
 
 @pytest.fixture()
-def ecoli_mlst_path(data_path):
+def ecoli_mlst_path(data_fpath):
     """Get path for ecoli mlst file"""
-    return str(data_path.joinpath("ecoli", "mlst.json"))
+    return str(data_fpath.joinpath("ecoli", "mlst.json"))
 
 
 @pytest.fixture()
-def ecoli_chewbbaca_path(data_path):
+def ecoli_chewbbaca_path(data_fpath):
     """Get path for ecoli chewbbaca file"""
-    return str(data_path.joinpath("ecoli", "chewbbaca.out"))
+    return str(data_fpath.joinpath("ecoli", "chewbbaca.out"))
 
 
 @pytest.fixture()
-def ecoli_bracken_path(data_path):
+def ecoli_bracken_path(data_fpath):
     """Get path for ecoli bracken file"""
-    return str(data_path.joinpath("ecoli", "bracken.out"))
+    return str(data_fpath.joinpath("ecoli", "bracken.out"))
 
 
 @pytest.fixture()
-def ecoli_cdm_input(data_path):
+def ecoli_cdm_input(data_fpath):
     """Get path for ecoli cdm file"""
-    path = data_path.joinpath("ecoli", "cdm_input.json")
+    path = data_fpath.joinpath("ecoli", "cdm_input.json")
     with open(path) as inpt:
         return json.load(inpt)
--- tests/fixtures/mtuberculosis/__init__.py
+++ tests/fixtures/mtuberculosis/__init__.py
@@ -2,37 +2,40 @@
 
 import pytest
 
+from ..fixtures import data_path
+
+
 @pytest.fixture()
-def mtuberculosis_analysis_meta_path(data_path):
+def mtuberculosis_analysis_meta_path(data_fpath):
     """Get path for mtuberculosis meta file"""
-    return str(data_path.joinpath("mtuberculosis", "analysis_meta.json"))
+    return str(data_fpath.joinpath("mtuberculosis", "analysis_meta.json"))
 
 
 @pytest.fixture()
-def mtuberculosis_bracken_path(data_path):
+def mtuberculosis_bracken_path(data_fpath):
     """Get path for mtuberculosis bracken file"""
-    return str(data_path.joinpath("mtuberculosis", "bracken.out"))
+    return str(data_fpath.joinpath("mtuberculosis", "bracken.out"))
 
 
 @pytest.fixture()
-def mtuberculosis_bwa_path(data_path):
+def mtuberculosis_bwa_path(data_fpath):
     """Get path for mtuberculosis bwa qc file"""
-    return str(data_path.joinpath("mtuberculosis", "bwa.qc"))
+    return str(data_fpath.joinpath("mtuberculosis", "bwa.qc"))
 
 
 @pytest.fixture()
-def mtuberculosis_mykrobe_path(data_path):
+def mtuberculosis_mykrobe_path(data_fpath):
     """Get path for mtuberculosis mykrobe file"""
-    return str(data_path.joinpath("mtuberculosis", "mykrobe.csv"))
+    return str(data_fpath.joinpath("mtuberculosis", "mykrobe.csv"))
 
 
 @pytest.fixture()
-def mtuberculosis_quast_path(data_path):
+def mtuberculosis_quast_path(data_fpath):
     """Get path for mtuberculosis quast file"""
-    return str(data_path.joinpath("mtuberculosis", "quast.tsv"))
+    return str(data_fpath.joinpath("mtuberculosis", "quast.tsv"))
 
 
 @pytest.fixture()
-def mtuberculosis_tbprofiler_path(data_path):
+def mtuberculosis_tbprofiler_path(data_fpath):
     """Get path for mtuberculosis tbprofiler file"""
-    return str(data_path.joinpath("mtuberculosis", "tbprofiler.json"))
+    return str(data_fpath.joinpath("mtuberculosis", "tbprofiler.json"))
