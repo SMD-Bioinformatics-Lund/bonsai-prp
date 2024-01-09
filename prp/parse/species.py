aa711f5b42d85622c1c2d709509fc10cae8671bb
mhkc <markus.johansson@me.com>
2024-01-09 11:31:09 +0100

    removed lower limit for including spp prediction res

--- prp/parse/species.py
+++ prp/parse/species.py
@@ -6,9 +6,6 @@ import pandas as pd
 LOG = logging.getLogger(__name__)
 
 
-SPP_MIN_READ_FRAC = 0.001
-
-
 def parse_kraken_result(file: str):
     """parse_species_pred""Parse species prediciton result"""
     tax_lvl_dict = {
@@ -26,8 +23,4 @@ def parse_kraken_result(file: str):
         .rename(columns=columns)
         .replace({"taxonomy_lvl": tax_lvl_dict})
     )
-    # limit the number of predicted species
-    species_pred = species_pred[
-        species_pred["fraction_total_reads"] > SPP_MIN_READ_FRAC
-    ]
     return species_pred.to_dict(orient="records")
