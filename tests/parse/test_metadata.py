2f4f2cafda581532ccb8a55413a52360d78c714b
mhkc <markus.johansson@me.com>
2024-09-03 11:56:50 +0200

    Added tests for parsing metadata

---
+++ tests/parse/test_metadata.py
@@ -0,0 +1,20 @@
+"""Test functions for parsing metadata."""
+
+from datetime import datetime
+from prp.parse.metadata import parse_sequence_date_from_run_id
+
+
+def test_parse_sequence_date_from_run_id():
+    """Test parsing of sequencing run id."""
+
+    # test that a run id from illumina works
+    date = parse_sequence_date_from_run_id("220214_NB501699_0302_AHJLM7AFX3")
+    assert date == datetime(2022, 2, 14)
+
+    # test that an unknown id does not work
+    date = parse_sequence_date_from_run_id("my-unknown-run-id")
+    assert date == None
+
+    # test that an unknown id does not work
+    date = parse_sequence_date_from_run_id("my_unknown_run_id")
+    assert date == None
