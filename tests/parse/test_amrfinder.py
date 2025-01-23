152e665c4291724dd1dfb2e96e80acf52d111d62
Markus Johansson <markus.h.johansson@skane.se>
2025-01-23 14:31:35 +0100

    Added more tests

--- tests/parse/test_amrfinder.py
+++ tests/parse/test_amrfinder.py
@@ -1,6 +1,6 @@
 """Virulencefinder parser test suite."""
 
-# import pytest
+import pytest
 
 from prp.parse.amrfinder import (
     AMRMethodIndex,
@@ -12,6 +12,7 @@ from prp.parse.amrfinder import (
 
 def test_parse_virulence_prediction(saureus_amrfinder_path):
     """Test parsing amrfinder resistance."""
+    # parse result
     result = parse_vir_pred(saureus_amrfinder_path)
 
     # test that result is method index
@@ -21,26 +22,26 @@ def test_parse_virulence_prediction(saureus_amrfinder_path):
     assert len(result.result.genes) == 14
 
 
-def test_parse_amr_prediction(saureus_amrfinder_path):
+EXPECTED_RESULT = [
+    ('saureus_amrfinder_path', (8, 3, ("beta-lactam", "fosfomycin", "methicillin", "quinolone", "tetracycline", "tigecycline"))),
+    ('saureus_amrfinder_no_amr_path', (0, 0, tuple()))
+]
+@pytest.mark.parametrize('fixture_name,expected', EXPECTED_RESULT)
+def test_parse_amr_prediction(fixture_name, expected, request):
     """Test parsing amrfinder resistance."""
-    result = parse_amr_pred(saureus_amrfinder_path)
+    exp_genes, exp_variants, exp_phenotypes = expected
+    filename = request.getfixturevalue(fixture_name)
+    # parse result
+    result = parse_amr_pred(filename)
 
     # test that result is method index
     assert isinstance(result, AMRMethodIndex)
 
     # test that all genes and variants are identified
-    assert len(result.result.genes) == 8
+    assert len(result.result.genes) == exp_genes
 
     # test that all genes and variants are identified
-    assert len(result.result.variants) == 3
+    assert len(result.result.variants) == exp_variants
 
     # test that all phenotypes have been reported
-    exp_phenotypes = { 
-        "beta-lactam",
-        "fosfomycin",
-        "methicillin",
-        "quinolone",
-        "tetracycline",
-        "tigecycline",
-    }
-    assert set(result.result.phenotypes['resistant']) == exp_phenotypes
+    assert set(result.result.phenotypes['resistant']) == set(exp_phenotypes)
