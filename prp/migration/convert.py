330a9d39029c2b68eb2043c3c2f98364eef5c59d
Markus Johansson <markus.h.johansson@skane.se>
2025-03-28 15:23:43 +0100

    Added migraion module

--- prp/migration/convert.py
+++ prp/migration/convert.py
@@ -1,6 +1,7 @@
-"""Functions to migrate from version 1 to version 2."""
+"""Functions to convert results to a new schema version."""
 
-from typing import Any
+from typing import Any, Callable
+from copy import copy
 import logging
 
 
@@ -8,10 +9,39 @@ from prp.models import PipelineResult
 
 LOG = logging.getLogger(__name__)
 
-type OldResult = dict[str, Any]
+type UnformattedResult = dict[str, Any]
 
 
-def v1_to_v2(old_result: OldResult) -> PipelineResult:
+def migrate_result(old_result: UnformattedResult) -> PipelineResult:
+    """Migrate old JASEN result to the current schema."""
+    ALL_FUNCS: dict[int, Callable[..., UnformattedResult]] = {2: v1_to_v2}
+
+    # verify input
+    input_schema_version = old_result['schema_version']
+    LOG.info("Migrating result from version %d", input_schema_version)
+    if input_schema_version not in ALL_FUNCS:
+        all_versions = ", ".join([str(ver) for ver in ALL_FUNCS])
+        raise ValueError(f"Unknown result version, found {input_schema_version} expected any of {all_versions}")
+    
+    # migrate 
+    temp_result = copy(old_result)
+    for to_version, func in ALL_FUNCS.items():
+        temp_result = func(temp_result)
+
+    return PipelineResult.model_validate(temp_result)
+
+
+def v1_to_v2(result: UnformattedResult) -> UnformattedResult:
     """Convert result in json format from v1 to v2."""
-
-    return
+    input_schema_version = result["schema_version"]
+    if input_schema_version != 1:
+        raise ValueError(f"Invalid schema version '{input_schema_version}' expected 1")
+    
+    LOG.info("Migrating from v%d to v%d", input_schema_version, 2)
+    upd_result = copy(result)
+    # split analysis profile into a list and strip white space
+    upd_profile: list[str] = [prof.strip() for prof in result['pipeline']['analysis_profile'].split(',')]
+    upd_result['pipeline']['analysis_profile'] = upd_profile
+    # update schema version
+    upd_result["schema_version"] = 2
+    return upd_result
