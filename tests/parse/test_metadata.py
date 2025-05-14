b753920b69252ea232442c679b121c7a768fa93d
Markus Johansson <markus.h.johansson@skane.se>
2025-05-14 09:22:31 +0200

    Added tests for metadata

--- tests/parse/test_metadata.py
+++ tests/parse/test_metadata.py
@@ -1,8 +1,12 @@
 """Test functions for parsing metadata."""
 
 from datetime import datetime
+from pathlib import Path
 
-from prp.parse.metadata import parse_date_from_run_id
+import yaml
+
+from prp.models.config import SampleConfig
+from prp.parse.metadata import parse_date_from_run_id, process_custom_metadata
 
 
 def test_parse_sequence_date_from_run_id():
@@ -19,3 +23,20 @@ def test_parse_sequence_date_from_run_id():
     # test that an unknown id does not work
     date = parse_date_from_run_id("my_unknown_run_id")
     assert date == None
+
+def test_process_custom_metadata(saureus_sample_conf_path: str):
+    """Test that custom metadata fields are handled properly."""
+    cnf_path = Path(saureus_sample_conf_path)
+    with cnf_path.open(encoding="utf-8") as cfile:
+        data = yaml.safe_load(cfile)
+    
+    # cast metadata records as expcected data type
+    cnf = SampleConfig.model_validate(data, context=saureus_sample_conf_path)
+    # run process function
+    proc_meta = process_custom_metadata(cnf.metadata)
+
+    # TEST that three records was returned
+    assert len(proc_meta) == 3
+
+    # TEST that TableMetadataEntry contains a stringed csv
+    assert isinstance(proc_meta[-1].value, str)
