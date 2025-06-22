e37f86f94db1f1aa971cc9377f1ef15e47257e64
ryanjameskennedy <ryanjameskennedy@icloud.com>
2025-06-22 23:01:59 +0200

    Fix bug when no refgenome provided

--- prp/models/config.py
+++ prp/models/config.py
@@ -31,8 +31,8 @@ class SampleConfig(RWModel):
     metadata: list[MetaEntry] = []
 
     # Reference genome
-    ref_genome_sequence: Path
-    ref_genome_annotation: Path
+    ref_genome_sequence: Path | None = None
+    ref_genome_annotation: Path | None = None
 
     igv_annotations: list[IgvAnnotation] = []
 
