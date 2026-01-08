aa589b37ea748000b48d18b082631a51e0b836d3
Markus Johansson <markus.h.johansson@skane.se>
2026-01-08 14:22:29 +0100

    Add basic parser registry

--- prp/parse/__init__.py
+++ prp/parse/__init__.py
@@ -1,5 +1,8 @@
 """Parse output of softwares in pipeline."""
 
+from importlib import import_module
+from pathlib import Path
+
 from .qc import (
     parse_alignment_results,
     parse_gambitcore_results,
@@ -9,3 +12,10 @@ from .qc import (
 from .sample import parse_sample
 from .typing import parse_cgmlst_results, parse_mlst_results
 from .variant import load_variants
+
+
+# auto-import all modules under parse/ to ensure that all parsers are registered
+_pkg_dir = Path(__file__).parent
+for file in _pkg_dir.glob("*.py"):
+    if file.name not in ("__init__.py", "registry.py"):
+        import_module(f"{__name__}.{file.stem}")
---
+++ prp/parse/registry.py
@@ -0,0 +1,49 @@
+"""Parser registry."""
+
+from packaging.version import Version
+from prp.models.registry import VersionRange,
+
+
+_REGISTRY: dict[str, list[VersionRange]] = {}
+
+
+def register_parser(software: str, min_version: str | None = None, max_version: str | None = None):
+    """Decorator to register a parser for a range of versions.
+    
+    Null values means either undefined or no upper range.
+    """
+    min_version = min_version or "0.0.0"
+    max_version = max_version or "99999.0.0"
+
+    def wrapper(cls):
+        range = VersionRange(
+            min_version=Version(min_version), max_version=Version(max_version), parser=cls
+        )
+        _REGISTRY.setdefault(software, []).append(range)
+    return wrapper
+
+
+def get_parser(software: str, version: str) -> Callable:
+    """Get parser from registry."""
+    version = Version(version)
+
+    if software not in registered_softwares():
+        raise ValueError(f"No parser registered for software: {software}")
+    
+    for span in sorted(_REGISTRY[software]):
+        if span.min_version <= version <= span.max_version:
+            return span.parser
+        
+    raise ValueError(f"No parser available for software '{software}' version {version}")
+
+
+def registered_softwares() -> list[str]:
+    """Get registered softwares."""
+
+    return list(_REGISTRY.keys())
+
+
+def registered_version_ranges(software: str) -> list[VersionRange]:
+    """Get ranges for registered software."""
+
+    return _REGISTRY.get(software, [])
