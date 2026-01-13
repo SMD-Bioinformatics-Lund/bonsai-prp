e8cb33bef0d0882a1eee7535f63d7eaafebf678d
Markus Johansson <markus.h.johansson@skane.se>
2026-01-13 15:12:23 +0100

    Improved tests

--- tests/parse/test_mykrobe.py
+++ tests/parse/test_mykrobe.py
@@ -2,6 +2,8 @@
 
 
 from prp.models.base import ParserOutput
+from prp.models.phenotype import ElementTypeResult
+from prp.models.typing import ResultLineageBase
 from prp.parse.mykrobe import MykrobeParser
 
 
@@ -14,6 +16,18 @@ def test_mykrobe_parser_results(mtuberculosis_mykrobe_path):
     # test that result is method index
     assert isinstance(result, ParserOutput)
 
-    # test that result contain the expected types 
+    # test that result contain the expected types
     result_types = list(result.results.keys())
     assert all(method in result_types for method in parser.produces)
+
+    # verify that species prediction was included
+    assert len(result.results["species"]) == 1
+    assert result.results["species"][0].scientific_name == "Mycobacterium tuberculosis"
+
+    # verify that lineage prediction
+    assert isinstance(result.results["lineage"], ResultLineageBase)
+
+    # verify that amr predictions
+    pred = result.results["amr"]
+    assert isinstance(pred, ElementTypeResult)
+    assert len(pred.variants) == 6
