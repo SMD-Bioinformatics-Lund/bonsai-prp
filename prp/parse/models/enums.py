8e19aae73a87816243af8b7c31afd97d3e93457e
Markus Johansson <markus.h.johansson@skane.se>
2026-02-11 16:08:11 +0100

    Moved AnalysisType and AnalysisSoftware to global

--- prp/parse/models/enums.py
+++ prp/parse/models/enums.py
@@ -2,62 +2,7 @@
 
 from enum import StrEnum
 
-
-class AnalysisSoftware(StrEnum):
-    """Single source of truth for analysis software names."""
-
-    AMRFINDER = "amrfinder"
-    BRACKEN = "bracken"
-    CHEWBBACA = "chewbbaca"
-    EMMTYPER = "emmtyper"
-    GAMBIT = "gambit"
-    HAMRONIZATION = "hAMRonization"
-    KLEBORATE = "kleborate"
-    MYKROBE = "mykrobe"
-    MLST = "mlst"
-    NANOPLOT = "nanoplot"
-    POSTALIGNQC = "postalignqc"
-    QUAST = "quast"
-    RESFINDER = "resfinder"
-    SAMTOOLS = "samtools"
-    SCCMECTYPER = "sccmectyper"
-    SEROTYPEFINDER = "serotypefinder"
-    SHIGAPASS = "shigapass"
-    SPATYPER = "spatyper"
-    TBPROFILER = "tbprofiler"
-    VIRULENCEFINDER = "virulencefinder"
-
-
-class AnalysisType(StrEnum):
-    """The various types of analysis a parser can produce."""
-
-    ABST = "abst"
-    AMR = "amr"
-    CBST = "cbst"
-    CGMLST = "cgmlst"
-    COVERAGE = "coverage"
-    EMM = "emm"
-    EMMTYPE = "emmtype"
-    H_TYPE = "h_type"
-    K_TYPE = "K_type"
-    LINEAGE = "lineage"
-    MLST = "mlst"
-    O_TYPE = "o_type"
-    QC = "qc"
-    RMST = "rmst"
-    SCCMEC = "sccmec"
-    SCCMECTYPE = "sccmectype"
-    SHIGATYPE = "shigatype"
-    SMST = "smst"
-    SPATYPE = "spatype"
-    SPECIES = "species"
-    STRESS = "stress"
-    STX = "stx"
-    TYPING = "typing"
-    VARIANT = "variant"
-    VIRULENCE = "virulence"
-    YBST = "ybst"
-
+from prp.models.enums import AnalysisSoftware, AnalysisType
 
 class ResultStatus(StrEnum):
     """
