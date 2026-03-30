4f9f783e9772d148da04ba932d7fc71a83b0501c
Markus Johansson <markus.h.johansson@skane.se>
2026-03-30 12:21:31 +0200

    Added tests for registering models and parsers

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 from prp.models.metadata import PipelineInfo, PipelineProvenance, PipelineRun, SequencingInfo
 from prp.models.sample import PipelineResult
+from prp.parse.core.registry import _PARSER_REGISTRY, _RESULT_MODEL_REGISTRY
 
 from .fixtures import *
 
@@ -12,6 +13,16 @@ from .fixtures import *
 def data_path() -> Path:
     """Get path of this file"""
     return Path(__file__).parent / "fixtures"
+
+
+@pytest.fixture(autouse=True)
+def clear_registry_before_each_test():
+    """Ensure a clean registry for each test."""
+    _PARSER_REGISTRY.clear()
+    _RESULT_MODEL_REGISTRY.clear()
+    yield
+    _PARSER_REGISTRY.clear()
+    _RESULT_MODEL_REGISTRY.clear()
 
 
 @pytest.fixture()
